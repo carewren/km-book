04a499f09ad600b55dc7fc3b406ad147b3e2f829
carewren <karenmmcc@gmail.com>
2019-08-06 02:31:18 -0600

    aug 6 update

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// let tile = document.getElementsByClassName("project-tile");
+
+// tile.addEventListener("mouseenter", function( event ) {
+//     event.target.class = "project-hover";
+// }, false);
